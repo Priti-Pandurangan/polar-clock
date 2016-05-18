eb9ea6acda8924a1855b3014d64b590195ca938f
Priti <pripra3014@gmail.com>
2016-05-19 04:39:01 +0530

    helper function to draw hand

--- js/clock.js
+++ js/clock.js
@@ -13,6 +13,22 @@ var centerY = canvas.height / 2
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 
+	drawHand(200, Math.PI / 4, "red")
+	drawHand(150, Math.PI / 4, "blue")
+	drawHand(100, Math.PI / 4, "green")
+}
+
+function drawHand(radius, theta, color, width) {
+	width = width || 20
+
+	ctx.beginPath()
+	ctx.arc(centerX, centerY, radius, 3 * Math.PI / 2, (3 * Math.PI / 2) + theta)
+	ctx.lineTo(centerX + (radius + width) * Math.cos((Math.PI / 2) - theta) , centerY - (radius + width) * Math.sin((Math.PI / 2) - theta))
+	ctx.arc(centerX, centerY, radius + width, (3 * Math.PI / 2) + theta, 3 * Math.PI / 2, true)
+	ctx.closePath()
+
+	ctx.fillStyle = color
+	ctx.fill()
 }
 
 setInterval(draw, 1000)
