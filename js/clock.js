addc9c97ad4d69d7217d2aa572da908ab9eb3a90
Priti <pripra3014@gmail.com>
2016-05-19 12:18:50 +0530

    added digital clock

--- js/clock.js
+++ js/clock.js
@@ -10,14 +10,16 @@ canvas.height = window.innerHeight
 var centerX = canvas.width / 2
 var centerY = canvas.height / 2
 
+var date
+var text
 var seconds = 0, minutes = 0, hours = 0;
 
 function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-	drawHand(200, (2 * Math.PI / 60) * seconds, "red")
-	drawHand(150, (2 * Math.PI / 60) * minutes, "blue")
-	drawHand(100, (2 * Math.PI / 12) * hours, "green")
+	drawHand(200, (2 * Math.PI / 60) * seconds, "#E26357")
+	drawHand(150, (2 * Math.PI / 60) * minutes, "#B4DC66")
+	drawHand(100, (2 * Math.PI / 12) * hours, "#CAF1EC")
 
 	if (seconds == 59) {
 		seconds = 0
@@ -30,11 +32,17 @@ function draw() {
 	} else {
 		seconds = seconds + 1
 	}
+
+	date = new Date()
+    text = getTime(date.getHours(), date.getMinutes(), date.getSeconds())
+
+    ctx.font = "30px Arial"
+    ctx.fillStyle = "black"
+    ctx.fillText(text, 140, 100)
 }
 
 function drawHand(radius, theta, color, width) {
 	width = width || 20
-
 	ctx.beginPath()
 	ctx.arc(centerX, centerY, radius, 3 * Math.PI / 2, (3 * Math.PI / 2) + theta)
 	ctx.lineTo(centerX + (radius + width) * Math.cos((Math.PI / 2) - theta) , centerY - (radius + width) * Math.sin((Math.PI / 2) - theta))
@@ -43,6 +51,24 @@ function drawHand(radius, theta, color, width) {
 
 	ctx.fillStyle = color
 	ctx.fill()
+}
+
+function getTime(hours, minutes, seconds) {
+    var currentTime
+    
+    if (hours < 10)
+        currentTime = "0" + hours.toString()
+    else
+        currentTime = hours.toString()
+    if (minutes < 10)
+        currentTime += ":0" + minutes.toString()
+    else
+        currentTime += ":" + minutes.toString()
+    if (seconds < 10) 
+        currentTime += ":0" + seconds.toString()
+    else
+        currentTime += ":" + seconds.toString()
+        return currentTime
 }
 
 setInterval(draw, 1000)
